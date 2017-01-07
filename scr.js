7eb77f9e577ae1955a1ca189c44f3df244a44408
nikki <zhenya_nikitenko@ukr.net>
2017-01-07 15:57:36 +0200

    es6 syntax remade

--- scr.js
+++ scr.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         marks: {
             user: 1,
-            computer: 0
+            computer: 0,
+            empty: -1
         },
         view: {
             user: 'x',
@@ -22,10 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
         sizeLength: null,
         sqWinCount: null,
         formedItems: null
-    }
+    };
 
     //начало игры
-    var begin = (event) => {
+    function begin(event) {
         event.preventDefault();
         /**
          * TODO: Хранить настройки и состояние игры в виде обьекта game = {
@@ -46,11 +47,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // создаем игровое поле и одномерный array игры
-var renderField = (size) => {
+    function renderField(size) {
         game.sizeLength = size;
         field.style.width = size * 102 + 'px';
         for (let i = 0; i < Math.pow(size, 2); i++) {
-            game.arrayField.push(-1);
+            game.arrayField.push(game.marks.empty);
             game.item = document.createElement('div');
             game.item.classList.add('item');
             field.appendChild(game.item);
@@ -58,13 +59,13 @@ var renderField = (size) => {
     }
 
     // делаем ход
-    var placeMark = (mark, b) => {
-        if (game.arrayField[mark] == -1) {
-            game.arrayField.splice(mark, 1, 1);
+    function placeMark(mark, b) {
+        if (game.arrayField[mark] == game.marks.empty) {
+            game.arrayField.splice(mark, 1, game.marks.user);
             while (b) {
                 game.pos = Math.floor(Math.random() * game.arrayField.length); // получаем рандомное число от 0 до 8
-                if (game.arrayField[game.pos] == -1) {
-                    game.arrayField.splice(game.pos, 1, 0);
+                if (game.arrayField[game.pos] == game.marks.empty) {
+                    game.arrayField.splice(game.pos, 1, game.marks.computer);
                     b = false;
                 }
             }
@@ -73,13 +74,13 @@ var renderField = (size) => {
     }
 
     // заполняем ячейки
-    var renderView = (arr) => {
+    function renderView(arr) {
         game.formedItems[game.clicked].innerHTML = game.view.user;
         game.formedItems[game.pos].innerHTML = game.view.computer;
     }
 
     // проверяем, остался один ход, или нет
-    var isLastMove = (c) => {
+    function isLastMove(c) {
         if (c >= game.arrayField.length - 1) {
             return true;
         } else {
@@ -93,21 +94,21 @@ var renderField = (size) => {
      */
 
     //определяем количество ячеек для победы
-    var squaresForWin = (n) => {
+    function squaresForWin(n) {
         game.sqWinCount = n;
     }
 
-    var reload = () => {
+    function reload() {
         location.reload(); // перезагружаем страницу
     }
 
     // координаты 2d массива в 1d
-    var getItem = (x, y) => {
+    function getItem(x, y) {
         return y * game.sizeLength + x;
     }
 
     //определяем длинну максимальную длинну последовательного вхождения символа в массив
-    var maxLength = (a, mark) => {
+    function maxLength(a, mark) {
         let c = 0;
         let maxlen = [];
         let max;
@@ -124,7 +125,7 @@ var renderField = (size) => {
     }
 
     // проверка на наличие победителя
-    var checkRow = (x, y, mark) => {
+    function checkRow(x, y, mark) {
         let arr = [];
         for (let i = 0; i < game.sizeLength; i++) {
             arr.push(game.arrayField[getItem(i, y)]);
@@ -132,7 +133,7 @@ var renderField = (size) => {
         return maxLength(arr, mark);
     }
 
-    var checkCol = (x, y, mark) => {
+    function checkCol(x, y, mark) {
         let arr = [];
         for (let i = 0; i < game.sizeLength; i++) {
             arr.push(game.arrayField[getItem(x, i)]);
@@ -140,23 +141,19 @@ var renderField = (size) => {
         return maxLength(arr, mark);
     }
 
-    var checkDiag1 = (x, y, mark) => {
+    function checkDiag1(x, y, mark) {
         let arr = [];
-        var x = (game.clicked - game.clicked % game.sizeLength) / game.sizeLength;
-        var y = game.clicked % game.sizeLength;
         let dif = y - x;
         let rdif = x - y;
         if (x == y) {
             for (let i = 0; i < game.sizeLength; i++) {
                 arr.push(game.arrayField[getItem(i, i)]);
             }
-        }
-        else if (y >= x) {
+        } else if (y >= x) {
             for (let i = dif; i < game.sizeLength; i++) {
                 arr.push(game.arrayField[getItem(i, (i - dif))]);
             }
-        }
-        else {
+        } else {
             for (let i = 0; i < (game.sizeLength - rdif); i++) {
                 arr.push(game.arrayField[getItem(i, (i + rdif))]);
             }
@@ -164,10 +161,8 @@ var renderField = (size) => {
         return maxLength(arr, mark);
     }
 
-    var checkDiag2 = (x, y, mark) => {
+    function checkDiag2(x, y, mark) {
         let arr = [];
-        var x = (game.clicked - game.clicked % game.sizeLength) / game.sizeLength;
-        var y = game.clicked % game.sizeLength;
         let max = x + y;
         if ((game.sizeLength - x - 1) == y) {
             for (let i = 0; i < game.sizeLength; i++) {
@@ -186,38 +181,38 @@ var renderField = (size) => {
     }
 
     // определеление победителя
-    var getWinner = () => {
-          var x = (game.clicked - game.clicked % game.sizeLength) / game.sizeLength;
-          var y = game.clicked % game.sizeLength;
-          var x1 = (game.pos - game.pos % game.sizeLength) / game.sizeLength;
-          var y1 = game.pos % game.sizeLength;
+    function getWinner() {
+        let x = (game.clicked - game.clicked % game.sizeLength) / game.sizeLength;
+        let y = game.clicked % game.sizeLength;
+        let x1 = (game.pos - game.pos % game.sizeLength) / game.sizeLength;
+        let y1 = game.pos % game.sizeLength;
 
-          let colU = checkCol(y, x, game.marks.user);
-          let rowU = checkRow(y, x, game.marks.user);
-          let diag1U = checkDiag1(y, x, game.marks.user);
-          let diag2U = checkDiag2(y, x, game.marks.user);
+        let colU = checkCol(y, x, game.marks.user);
+        let rowU = checkRow(y, x, game.marks.user);
+        let diag1U = checkDiag1(x, y, game.marks.user);
+        let diag2U = checkDiag2(x, y, game.marks.user);
 
-          let colC = checkCol(y1, x1, game.marks.computer);
-          let rowC = checkRow(y1, x1, game.marks.computer);
-          let diag1C = checkDiag1(y1, x1, game.marks.computer);
-          let diag2C = checkDiag2(y1, x1, game.marks.computer);
+        let colC = checkCol(y1, x1, game.marks.computer);
+        let rowC = checkRow(y1, x1, game.marks.computer);
+        let diag1C = checkDiag1(x1, y1, game.marks.computer);
+        let diag2C = checkDiag2(x1, y1, game.marks.computer);
 
-          if (colU || rowU || diag1U || diag2U) {
-              game.win = true;
-              game.winner = game.players.user;
-          } else if (colC || rowC || diag1C || diag2C) {
-              game.win = true;
-              game.winner = game.players.computer;
-          } else if (game.count > Math.pow(game.sizeLength, 2) - 1) {
-              alert('draw');
-              reload();
-          }
-          if (game.win) {
-              field.style.display = 'none';
-              finisBox.style.display = 'block';
-              finisBox.getElementsByTagName('h1')[0].innerHTML += game.winner;
-          }
+        if (colU || rowU || diag1U || diag2U) {
+            game.win = true;
+            game.winner = game.players.user;
+        } else if (colC || rowC || diag1C || diag2C) {
+            game.win = true;
+            game.winner = game.players.computer;
+        } else if (game.count > Math.pow(game.sizeLength, 2) - 1) {
+            alert('draw');
+            reload();
         }
+        if (game.win) {
+            field.style.display = 'none';
+            finisBox.style.display = 'block';
+            finisBox.getElementsByTagName('h1')[0].innerHTML += game.winner;
+        }
+    }
 
     const field = document.querySelector('.field'); // TODO: проще document.querySelector('.field'), найдет первый элемент по запрошенному селектору
     const form = document.getElementById('gameform'); // TODO: не сказал бы, что это хорошая практика. Лучше уже document.getElementsById.
